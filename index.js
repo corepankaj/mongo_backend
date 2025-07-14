ffb035945aa35783171677dc9852afff0f173a5f
Pankaj Ranjan Sinha <corepankaj@gmail.com>
2025-07-14 14:45:33 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config({path:"./.env"})
 const mongoose = require("mongoose");
 const app = express();
 const studentRoutes = require("./routes/students.routes");
 
-mongoose.connect("mongodb://localhost:27017/students-crud")
+
+
+mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
     console.log("Connected to mongoDB");
 
@@ -21,6 +24,6 @@ app.use(express.json())
 
 app.use("/api/students",studentRoutes);
 
-app.listen(8000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("Server runing on port no 80000");
 })
--- models/students.model.js
+++ models/students.model.js
@@ -9,5 +9,16 @@ const studentSceama = new mongoose.Schema({
         require: true
     }
 })
-const Student = mongoose.model("Student", studentSceama);
+const Student = mongoose.model("Studentinfo", studentSceama);
+
+  // 4. Insert a test document
+    /*Student.create({
+      name: "Pankaj Ranjan Sinha",
+      phone: "9654066919"
+    }).then(() => {
+      console.log("✅ Document inserted");
+    }).catch(err => {
+      console.error("Insert error:", err);
+    });*/
+
 module.exports = Student;
